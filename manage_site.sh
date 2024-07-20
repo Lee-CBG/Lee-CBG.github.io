2facaf87a7fe01e5f5fe76c49941e250188b2a75
techsavvy <piyusshsinghal01@gmail.com>
2024-07-20 11:36:38 -0700

    scripts build test2

--- manage_site.sh
+++ manage_site.sh
@@ -8,37 +8,41 @@ function show_menu() {
     echo "3. Research"
     echo "4. Publications"
     echo "5. Exit"
-    read -p "Enter your choice [1-5]: " choice
+    echo "Enter your choice [1-5]: "
+    read choice
     return $choice
 }
 
 # Function to execute the Python script based on the user's choice
 function execute_script() {
+    script_path="scripts"
     case $1 in
         1)
             echo "Modifying Teams..."
-            python3 scripts/teams.py
+            python3 $script_path/teams.py
             ;;
         2)
             echo "Modifying News..."
-            python3 scripts/news.py
+            python3 $script_path/news.py
             ;;
         3)
             echo "Modifying Research..."
-            python3 scripts/research.py
+            python3 $script_path/research.py
             ;;
         4)
             echo "Modifying Publications..."
-            python3 scripts/publications.py
+            python3 $script_path/publications.py
             ;;
         5)
             echo "Exiting..."
             exit 0
             ;;
         *)
-            echo "Invalid choice..."
+            echo "Invalid choice. Please select a valid option."
+            return 1
             ;;
     esac
+    return 0
 }
 
 # Function to handle Git operations
@@ -46,6 +50,8 @@ function git_operations() {
     read -p "Do you want to push these changes to the repository? (y/n): " answer
     if [[ $answer = "y" || $answer = "Y" ]]
     then
+        echo "Running build command before pushing changes. Uno Momento!"
+        bundle exec jekyll build
         git add .
         read -p "Enter commit message: " commit_message
         git commit -m "$commit_message"
@@ -62,7 +68,8 @@ do
     show_menu
     choice=$?
     execute_script $choice
-    if [[ $choice -ge 1 && $choice -le 4 ]]
+    script_status=$?
+    if [[ $script_status -eq 0 && $choice -ne 5 ]]
     then
         git_operations
     fi
